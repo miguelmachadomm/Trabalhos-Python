ee24d77a8a998d9c25eb2809265450e9922354e6
miguelmachadomm <miguellodige@gmail.com>
2023-08-07 20:55:15 -0300

    first commit

---
+++ atividade.py
@@ -0,0 +1,15 @@
+nome = "miguel machado"
+idade = 2023-2003
+altura = 1,85 
+peso = 84
+
+
+print (nome)
+print (str(idade) + "anos")
+print (idade , "anos")
+print (str(altura) + "altura")
+print (str(peso) + "peso")
+if idade >= 18: 
+    print ("voce é maior de idade!.")
+else : 
+    print(" voce é menor de idade!.")
